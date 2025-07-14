6568e980d1081292f0f1031b5938bac92a9d070e
Edoardo Lova <edoardolova81@gmail.com>
2025-07-14 18:34:24 +0200

    add dinamic cration for navbarexplore

--- components/NavbarExplore.jsx
+++ components/NavbarExplore.jsx
@@ -1,31 +1,44 @@
-export default function NavbarExplore(){
-    return(
-        <>
-            <div className="container text-center navabarExplore py-5">
-                <div className="row">
-                    <div className="col">
-                        <img src="\img\buy-comics-digital-comics.png" alt="" className="me-3"/>
-                        <a href="#">DIGITAL COMICS</a>
-                    </div>
-                    <div className="col">
-                        <img src="\img\buy-comics-merchandise.png" alt="" className="me-3"/>
-                        <a href="#">DC MERCHANDISE</a>
-                    </div>
-                    <div className="col">
-                        <img src="\img\buy-comics-subscriptions.png" alt="" className="me-3"/>
-                        <a href="#">SUBSCRIPTION</a>
-                    </div>
-                    <div className="col">
-                        <img src="\img\buy-comics-shop-locator.png" alt="" className="me-3"/>
-                        <a href="#">COMIC SHOP LOCATOR</a>
-                    </div>
-                    <div className="col">
-                        <img src="\img\buy-dc-power-visa.svg" alt="" className="me-3"/>
-                        <a href="#">DC POWER VISA</a>
-                    </div>
+export default function NavbarExplore() {
+    const navItems = [
+        {
+            imgSrc: "\\img\\buy-comics-digital-comics.png",
+            text: "DIGITAL COMICS",
+            link: "#"
+        },
+        {
+            imgSrc: "\\img\\buy-comics-merchandise.png",
+            text: "DC MERCHANDISE",
+            link: "#"
+        },
+        {
+            imgSrc: "\\img\\buy-comics-subscriptions.png",
+            text: "SUBSCRIPTION",
+            link: "#"
+        },
+        {
+            imgSrc: "\\img\\buy-comics-shop-locator.png",
+            text: "COMIC SHOP LOCATOR",
+            link: "#"
+        },
+        {
+            imgSrc: "\\img\\buy-dc-power-visa.svg",
+            text: "DC POWER VISA",
+            link: "#"
+        }
+    ];
 
+    return (
+        <>
+            <div className="container text-center navbarExplore py-5">
+                <div className="row">
+                    {navItems.map((item) => (
+                        <div className="col">
+                            <img src={item.imgSrc} alt={item.text} className="me-3" />
+                            <a href={item.link}>{item.text}</a>
+                        </div>
+                    ))}
                 </div>
             </div>
         </>
-    )
+    );
 }
