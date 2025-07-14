bff47f6f5877dcdd9d88d674ce658a3d01593b1c
Edoardo Lova <edoardolova81@gmail.com>
2025-07-14 15:44:06 +0200

    add content section

---
+++ components/Main.jsx
@@ -0,0 +1,12 @@
+export default function Main(){
+    return(
+        <>
+            <div className="contentDiv">
+                <div className="container">
+                    <h2>Content goes here</h2>
+                </div>
+            </div>
+        
+        </>
+    )
+}
